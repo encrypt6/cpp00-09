a54cd89d6f3cbb35c2a22ca08be8a692cba57803
Eliza Sikira <sikiraeliza2@gmail.com>
2025-05-20 00:00:37 +0200

    retro_phonebook

---
+++ ex01/retro_phonebook.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   retro_phonebook.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: elsikira <elsikira@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/19 23:52:26 by elsikira          #+#    #+#             */
+/*   Updated: 2025/05/19 23:59:15 by elsikira         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string>
+
+class	Contact
+{
+	private:
+		std::string firstName;
+		std::string lastName;
+		std::string nickName;
+		std::string phoneNumber;
+		std::string darkestSecret;
+	
+	public:
+
+}
