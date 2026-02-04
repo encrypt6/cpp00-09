a17bba9e316789ace30741f624a303903437d0fa
Eliza Sikira <elsikira@f4r10s16.clusters.42paris.fr>
2026-02-05 00:01:29 +0100

    gnah

---
+++ cpp04/ex00/Animal.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Animal.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: elsikira <elsikira@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/03 20:23:38 by elsikira          #+#    #+#             */
+/*   Updated: 2025/10/03 21:18:08 by elsikira         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ANIMAL_HPP
+#define ANIMAL_HPP
+
+class	Animal
+{
+	private:
+		std::string	type;
+
+	public:
+		Animal();
+		Animal(const std::string str);
+		Animaloperator=
+}
+
+#endif
